e2ea58ca83bc45ff1118da9e1121a7c7ef334162
Luana Matias Terense <luana.r.matias@gmail.com>
2023-01-04 09:51:53 -0300

    Rename do arquivo de teste + Configuração do Cypress Cloud no Cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://countries.trevorblades.com/',
+    baseUrl: 'https://snowtooth.moonhighway.com/graphql',
+    projectId: "xh7k56",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
